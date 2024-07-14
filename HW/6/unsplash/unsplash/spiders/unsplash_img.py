15d112a06782acf5753cbbfc1dd6b2f89f31cfce
Mindmech <alex8008@bk.ru>
2024-07-14 14:51:45 +0300

    hw6 done

--- HW/6/unsplash/unsplash/spiders/unsplash_img.py
+++ HW/6/unsplash/unsplash/spiders/unsplash_img.py
@@ -6,12 +6,12 @@ import scrapy.resolver
 class UnsplashImg(scrapy.Spider):
     name = "unsplash"
     allowed_domains = ["unsplash.com"]
-    start_urls = "https://unsplash.com/t/wallpapers"
-    short_base = "https://unsplash.com/"
+    start_urls = ["https://unsplash.com/t/wallpapers"]
+    count = 0
 
     def parse(self, response):
         for image_page in response.xpath(".//figure[@itemtype='https://schema.org/ImageObject']/a/@href").extract():
-            yield scrapy.Request(response.urljoin("https://unsplash.com/", image_page), self.parse_image_page)
+            yield scrapy.Request(urljoin("https://unsplash.com/", image_page), self.parse_image_page)
 
     def parse_image_page(self, response):
         full_url = response.xpath(".//div[@data-test='photos-route']//div/div[1]/div[2]/div/img[2]/@src").get()
@@ -19,6 +19,9 @@ class UnsplashImg(scrapy.Spider):
             yield scrapy.Request(full_url, self.save_image)
 
     def save_image(self, response):
-        filename = response.url.split('/')[-1]
-        with open (f'images/{filename}', 'wb') as f:
+        name = response.url.split('/')[-1]
+        name = name.split('?fm=jpg')
+        name = name[:1]
+        name = ' '.join(name)
+        with open (f'images\\{name}.jpg','wb') as f:
             f.write(response.body)
